bf802998dd0436f1b0c7f6d2ff9e200a44da2231
Yauheni Lebedzeu <lemot@yandex.ru>
2024-12-04 15:35:05 +0300

    KS-1-4 add a solution of the task 4

---
+++ src/main/kotlin/lesson_1/lesson_1_task_4.kt
@@ -0,0 +1,19 @@
+package com.lemot.lesson_1
+
+fun main() {
+
+    val length: Long = 40_868_600_000
+    val age: Byte = 27
+    val dayPart: Float = 0.075F
+    val seconds: Short = 6480
+    val yearPart: Double = 2.0547945205479453E-4
+    val orbitApogee: Int = 327000
+
+    println(length)
+    println(age)
+    println(dayPart)
+    println(seconds)
+    println(yearPart)
+    println(orbitApogee)
+
+}
