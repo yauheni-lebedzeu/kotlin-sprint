f4d371742f491d34788ed6aceea710bef2b1081e
Yauheni Lebedzeu <lemot@yandex.ru>
2024-12-04 15:04:37 +0300

    KS-1-2 add a solution of the task 2

---
+++ src/main/kotlin/lesson_1/lesson_1_task_2.kt
@@ -0,0 +1,18 @@
+package com.lemot.lesson_1
+
+fun main() {
+
+    val numberOfOrders: Int = 75
+    val thanksForPurchaseText: String = "Great thanks for purchasing!"
+
+    println(numberOfOrders)
+    println(thanksForPurchaseText)
+
+    var numberOfEmployees: Int = 2000
+
+//    println(numberOfEmployees)
+
+    numberOfEmployees = 1999
+
+    println(numberOfEmployees)
+}
