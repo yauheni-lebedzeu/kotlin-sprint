e06d9f34725066c14d35326e2b5b70b30ea1ac1f
Yauheni Lebedzeu <lemot@yandex.ru>
2024-12-05 16:34:52 +0300

    KS-1-5 make code refactoring of the task 5

--- src/main/kotlin/lesson_1/lesson_1_task_5.kt
+++ src/main/kotlin/lesson_1/lesson_1_task_5.kt
@@ -1,17 +1,23 @@
 package com.lemot.lesson_1
 
+private const val MINUTES_IN_HOUR = 60
+private const val SECONDS_IN_MINUTES = 60
+
 fun main() {
+
     val secondsInSpace = 6480
-    val fullMinutes = secondsInSpace / 60
-    val hours = fullMinutes / 60
-    val minutes = fullMinutes % 60
-    val seconds = secondsInSpace % 60
+    val fullMinutes = secondsInSpace / SECONDS_IN_MINUTES
+    val hours = fullMinutes / MINUTES_IN_HOUR
+    val minutes = fullMinutes % MINUTES_IN_HOUR
+    val seconds = secondsInSpace % SECONDS_IN_MINUTES
 
     val hourString = getTimeUnitString(hours)
     val minutesString = getTimeUnitString(minutes)
     val secondsString = getTimeUnitString(seconds)
 
-    println("$hourString:$minutesString:$secondsString")
+    val resultTimeString = String.format("%s:%s:%s", hourString, minutesString, secondsString)
+
+    println(resultTimeString)
 
 }
 
