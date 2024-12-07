b3f083ed5308b25c0fa0286e1fb03c122d4064b9
Yauheni Lebedzeu <lemot@yandex.ru>
2024-12-07 08:54:36 +0300

    KS-2-3 make a code refactoring

--- src/main/kotlin/lesson_2/lesson_2_task_3.kt
+++ src/main/kotlin/lesson_2/lesson_2_task_3.kt
@@ -8,21 +8,15 @@ fun main() {
     val departureHours = 9
     val departureMinutes = 39
     var onWayMinutes = 457
-
     val onWayHours = onWayMinutes / MINUTES_IN_HOUR
     onWayMinutes %= MINUTES_IN_HOUR
 
     var arrivalHour = departureHours + onWayHours
     var arrivalMinutes = departureMinutes + onWayMinutes
 
-    if (arrivalMinutes >= MINUTES_IN_HOUR) {
-        arrivalHour += arrivalMinutes / MINUTES_IN_HOUR
-        arrivalMinutes %= MINUTES_IN_HOUR
-    }
-
-    if (arrivalHour >= HOURS_IN_DAY) {
-        arrivalHour %= HOURS_IN_DAY
-    }
+    arrivalHour += arrivalMinutes / MINUTES_IN_HOUR
+    arrivalMinutes %= MINUTES_IN_HOUR
+    arrivalHour %= HOURS_IN_DAY
 
     println(String.format("Время прибытия поезда: %02d:%02d", arrivalHour, arrivalMinutes))
 }
