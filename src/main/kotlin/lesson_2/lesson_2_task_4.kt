d07510e637f0ae44f5cfb345917c960e92d4a7ee
Yauheni Lebedzeu <lemot@yandex.ru>
2024-12-07 08:46:04 +0300

    KS-2-4 make a code refactoring

--- src/main/kotlin/lesson_2/lesson_2_task_4.kt
+++ src/main/kotlin/lesson_2/lesson_2_task_4.kt
@@ -1,14 +1,14 @@
 package com.lemot.lesson_2
 
-private const val BUFF_VALUE = 0.2f
+private const val BUFF_PERCENTAGE = 20
 
 fun main() {
 
     val cristalOreQuantity = 7
     val ironOreQuantity = 11
 
-    val cristalOreBonus = (cristalOreQuantity * BUFF_VALUE).toInt()
-    val ironOreBonus = (ironOreQuantity * BUFF_VALUE).toInt()
+    val cristalOreBonus = (cristalOreQuantity * (BUFF_PERCENTAGE / 100f)).toInt()
+    val ironOreBonus = (ironOreQuantity * (BUFF_PERCENTAGE / 100f)).toInt()
 
     println("Бонус кристаллической руды: $cristalOreBonus")
     println("Бонус железной руды: $ironOreBonus")
