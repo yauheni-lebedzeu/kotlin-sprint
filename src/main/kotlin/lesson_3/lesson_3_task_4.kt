602aea66324ae86e86dd39328c8a011f8e01f58a
Yauheni Lebedzeu <lemot@yandex.ru>
2024-12-10 00:02:59 +0300

    KS-3-4 make a solution of the task 4 of the lesson 3

---
+++ src/main/kotlin/lesson_3/lesson_3_task_4.kt
@@ -0,0 +1,17 @@
+package com.lemot.lesson_3
+
+fun main() {
+
+    var fromPosition = "E2"
+    var toPosition = "E4"
+    var moveNumber = 1
+
+    println("$fromPosition-$toPosition; $moveNumber")
+
+    fromPosition = "D2"
+    toPosition = "D3"
+    moveNumber++
+
+    println("$fromPosition-$toPosition; $moveNumber")
+
+}
