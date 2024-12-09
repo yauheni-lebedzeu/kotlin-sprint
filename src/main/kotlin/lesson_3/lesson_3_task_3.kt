86cb512cbfc46364232b4ffd6f86a0ad77276468
Yauheni Lebedzeu <lemot@yandex.ru>
2024-12-09 14:13:24 +0300

    KS-3-3 make a solution of the task 3 of the lesson 3

---
+++ src/main/kotlin/lesson_3/lesson_3_task_3.kt
@@ -0,0 +1,20 @@
+package com.lemot.lesson_3
+
+fun main() {
+
+    val number = 6
+
+    println(
+        """
+            $number X 2 = ${number * 2}
+            $number X 3 = ${number * 3}
+            $number X 4 = ${number * 4}
+            $number X 5 = ${number * 5}
+            $number X 6 = ${number * 6}
+            $number X 7 = ${number * 7}
+            $number X 8 = ${number * 8}
+            $number X 9 = ${number * 9}
+        """.trimIndent()
+    )
+
+}
