f97ae410f34e3b736432ad7c40479cb8971784cc
Yauheni Lebedzeu <lemot@yandex.ru>
2024-12-11 10:06:57 +0300

    KS-3-4 fix an issue

--- src/main/kotlin/lesson_3/lesson_3_task_4.kt
+++ src/main/kotlin/lesson_3/lesson_3_task_4.kt
@@ -6,12 +6,12 @@ fun main() {
     var toPosition = "E4"
     var moveNumber = 1
 
-    println("$fromPosition-$toPosition; $moveNumber")
+    println("$fromPosition-$toPosition;$moveNumber")
 
     fromPosition = "D2"
     toPosition = "D3"
     moveNumber++
 
-    println("$fromPosition-$toPosition; $moveNumber")
+    println("$fromPosition-$toPosition;$moveNumber")
 
 }
